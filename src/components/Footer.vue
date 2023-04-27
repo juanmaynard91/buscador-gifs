d91fc5f9c37a0cff77fb9aee985d6da8f9b98ff4
juanmaynard91 <juanmaynard91@gmail.com>
2023-04-26 21:56:09 -0300

    agregando boton para compartir proyecto

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="footer">
     <footer class="bg-dark text-white py-1">
-      <h3 class="text-center">{{ fecha }} - HECHO POR JUAN MAYNARD</h3>
+      <!--<h3 class="text-center">{{ fecha }} - HECHO POR JUAN MAYNARD</h3>-->
+      <button class="btn btn-success" @click="compartir">Compartir proyecto <i class="bi bi-share-fill"></i></button>
     </footer>
   </div>
 </template>
 
 <script setup>
-const fecha = new Date().getFullYear();
+//const fecha = new Date().getFullYear();
+
+const compartir = () => {
+  if (navigator.share) {
+    navigator.share({
+      title: "compartir en redes sociales",
+      text: "gracias pa :D",
+      url: "https://singular-duckanoo-d57f03.netlify.app/#/",
+    });
+  } else {
+    navigator.clipboard.writeText("link copiado!");
+  }
+};
 </script>
 
 <style>
