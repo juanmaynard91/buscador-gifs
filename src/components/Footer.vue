5894cab63151af0adaf64b91877b1613d80df2fb
juanmaynard91 <juanmaynard91@gmail.com>
2023-04-26 23:32:07 -0300

    quitando codigo comentado

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,25 +2,12 @@
   <div class="footer">
     <footer class="bg-dark text-white p-3">
       <h3 class="text-center m-0">{{ fecha }} - JUAN MAYNARD</h3>
-      <!--<button class="btn btn-success" @click="compartir">Compartir proyecto <i class="bi bi-share-fill"></i></button>-->
     </footer>
   </div>
 </template>
 
 <script setup>
 const fecha = new Date().getFullYear();
-
-/*const compartir = () => {
-  if (navigator.share) {
-    navigator.share({
-      title: "compartir en redes sociales",
-      text: "gracias pa :D",
-      url: "https://github.com/juanmaynard91/buscador-gifs",
-    });
-  } else {
-    navigator.clipboard.writeText("link copiado!");
-  }
-};*/
 </script>
 
 <style>
